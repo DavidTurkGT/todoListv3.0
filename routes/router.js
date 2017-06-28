81a262aabd14c80cf1e2a3279f6004b6a9346965
DavidTurkGT <dlturk747@gmail.com>
2017-06-28 14:56:06 -0400

    Added ability to add a new task

--- routes/router.js
+++ routes/router.js
@@ -2,21 +2,49 @@ const express = require('express');
 const router = express.Router();
 const models =  require("../models");
 
+let todos = [];
+let errors = {};
+
 router.get("/", function(req, res){
   res.redirect("/todo");
 });
 
 router.get("/todo", function(req, res){
-  let todos = [];
   models.todos.findAll().then(function(data){
-    console.log("Data received: ", data);
     data.forEach(function(datum){
-      console.log("Add the value: ",datum.dataValues);
       todos.push(datum.dataValues)
     });
-    console.log("Todo list: ",todos);
-    res.render("todo", { todos: todos});
+    res.render("todo", { todoList: todos, error: errors});
   });
+});
+
+router.post("/todo/add", function(req,res){
+  todos = [];
+  errors = {};
+  req.checkBody("todo", "No text inputted for task").notEmpty();
+
+  req.getValidationResult().then(function(result){
+    if(result.isEmpty()){
+      let newTodo = {
+        task: req.body.todo,
+        complete: false
+      };
+      console.log("New task to add to DB: ", newTodo);
+      models.todos.create(newTodo).then(function(){
+        res.redirect("/todo");
+      });
+    }
+    else{
+      errors = result.mapped();
+      res.redirect("/todo");
+    }
+  })
+
+});
+
+router.post("todo/:itemID/complete", function(req, res){
+  console.log("Marking todo item complete...");
+  res.redirect("/todo");
 })
 
 module.exports = router;
